7886947a8ae9b47c0692909a1b6fe993ad521f4f
hyeonsik-park <gustlr4372@naver.com>
2024-08-15 16:38:41 +0900

    비밀번호 해쉬

---
+++ src/utils/hash.py
@@ -0,0 +1,9 @@
+# src/utils/hash.py
+import bcrypt
+
+def hash_password(password: str) -> str:
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed.decode('utf-8')
+
+def check_password(password: str, hashed: str) -> bool:
+    return bcrypt.checkpw(password.encode('utf-8'), hashed.encode('utf-8'))
